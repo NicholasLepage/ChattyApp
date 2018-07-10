4acee4bc00ac1e9e1d6ad0efd031237a45ea0a4e
Nicholas Lepage <nic.lep@hotmail.com>
2018-07-10 16:54:04 +0000

    Updated component readability + added initial state + passed props to MessageList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,46 @@
-import React, {Component} from 'react';
-import ChatBar from './ChatBar.jsx';
-import MessageList from './MessageList.jsx';
+import React, { Component } from "react";
+import ChatBar from "./ChatBar.jsx";
+import MessageList from "./MessageList.jsx";
+import Navbar from "./Navbar.jsx";
 
 class App extends Component {
+
+  state = {
+    currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+    messages: [
+      {
+        id: "f74h9r",
+        username: "Bob",
+        content: "Has anyone seen my marbles?",
+      },
+      {
+        id: "ref983",
+        username: "Anonymous",
+        content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+      }
+    ]
+}
+
+  componentDidMount() {
+    console.log("componentDidMount <App />");
+    setTimeout(() => {
+      console.log("Simulating incoming message");
+      // Add a new message to the list of messages in the data store
+      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+      const messages = this.state.messages.concat(newMessage)
+      // Update the state of the app component.
+      // Calling setState will trigger a call to render() in App and all child components.
+      this.setState({messages: messages})
+    }, 3000);
+  }
+
+
   render() {
     return (
       <div>
-        <nav className="navbar">
-          <a href="/" className="navbar-brand">Chatty</a>
-        </nav>
-        <MessageList />
-        <ChatBar />
+        <Navbar />
+        <MessageList messages={this.state.messages}/>
+        <ChatBar username={this.state.currentUser.name}/>
       </div>
     );
   }
--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -3,13 +3,20 @@ import Message from './Message.jsx';
 
 
 class MessageList extends Component {
+
   render() {
+
+    const fullMessageList = this.props.messages.map((message) => {
+      return <div key={message.id} className="message">
+               <span className="message-username">{message.username}</span>
+               <span className="message-content">{message.content}</span>
+             </div>
+    })
+
     return (
       <main className="messages">
         <Message />
-        <div className="message system">
-          Anonymous1 changed their name to nomnom.
-        </div>
+        {fullMessageList}
       </main>
     );
   }
